a1d9c1f412256461f7419ae91cc9bb9e678bb92c
Rodhis <rodrigonoliva@gmail.com>
2023-05-02 20:14:31 -0300

    add public path

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/hotel-complete-systems/" : "/",
+  publicPath: '/hotel-complete-systems/'
 };
